8a030c27fde31e1c9d18002595a3d42356871370
Andrew Archibald <andrew@aarchibald.com>
2021-04-02 01:17:02 -0600

    Add go.mod, fix test

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/andrewarchi/bvg-archive
+
+go 1.15
+
+require (
+	github.com/PuerkitoBio/goquery v1.6.1
+	golang.org/x/net v0.0.0-20210331212208-0fccb6fa2b5c
+)
--- wayback/timemap_test.go
+++ wayback/timemap_test.go
@@ -1,19 +1,28 @@
 package wayback
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
 
 func TestGetTimeMap(t *testing.T) {
 	timemap, err := GetTimeMap("https://www.bvg.de/de/Fahrinfo/Downloads/BVG-Liniennetz")
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Error(timemap)
+	fmt.Println(timemap)
 }
 
 func TestGetPage(t *testing.T) {
-	body, err := GetPage("https://www.bvg.de/de/Fahrinfo/Downloads/BVG-Liniennetz", "20200229212100")
+	resp, err := GetPage("https://www.bvg.de/de/Fahrinfo/Downloads/BVG-Liniennetz", "20200229212100")
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Error(string(body))
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(string(b))
 }
